018c413a2d95de98be13ff0c5f6d9e0dc5d8f1cb
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-29 17:29:52 +0530

    Add styles

--- src/component/Header.js
+++ src/component/Header.js
@@ -16,10 +16,10 @@ const menuItems = [
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
   return (
-    <header className={`flex flex-col bg-primaryColor px-8 py-7 ${mobileMenu ? 'h-screen' : 'h-fit'} fixed top-0 w-screen shadow-xl md:flex-row md:justify-between md:px-20 md:h-fit`}>
-      <a href="/" className="text-lg">Book Appointents App</a>
+    <header className={`flex flex-col bg-lime-500 px-8 py-7 ${mobileMenu ? 'h-screen' : 'h-fit'} fixed top-0 w-screen shadow-xl md:flex-row md:justify-between md:px-20 md:h-fit`}>
+      <a href="/" className="text-lg text-slate-50">Book Appointents App</a>
       <nav>
-        <div className="text-secondaryColor md:hidden mobile-btns">
+        <div className="text-slate-50 md:hidden mobile-btns">
           <button
             type="button"
             className={classNames(!mobileMenu ? 'block' : 'hidden')}
@@ -32,7 +32,7 @@ const Header = () => {
           </button>
         </div>
         <div className={classNames(mobileMenu ? 'block' : 'hidden', 'md:block')}>
-          <ul className="flex flex-col text-skyColor text-lg gap-8 items-center mt-24 md:flex-row md:mt-1">
+          <ul className="flex flex-col text-slate-50 text-lg gap-8 items-center mt-24 md:flex-row md:mt-1">
             {
               menuItems.map((item) => (
                 <li key={item.name}>
