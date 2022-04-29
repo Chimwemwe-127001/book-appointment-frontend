13dcbdf4b22f491d92bc54d42391e20d8b6ede66
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-29 20:17:53 +0530

    Fix bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,16 @@
-import React, { useState } from "react";
-import SideBar from "../components/SideBar";
+import React from 'react';
+import SideBar from '../components/SideBar';
 
-const Home = () => {
-
-  return (
-    <div className="h-screen flex overflow-hidden bg-white">
-      {/* Static sidebar for desktop */}
-      <div className="hidden lg:flex lg:flex-shrink-0">
-        <SideBar />
-      </div>
-      <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
-        hello there
-      </div>
+const Home = () => (
+  <div className="h-screen flex overflow-hidden bg-white">
+    {/* Static sidebar for desktop */}
+    <div className="hidden lg:flex lg:flex-shrink-0">
+      <SideBar />
     </div>
-  );
-};
+    <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
+      hello there
+    </div>
+  </div>
+);
 
 export default Home;
