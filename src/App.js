839f6fea681ff38c915d02c05eb0adc93ded6348
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-29 15:36:21 +0530

    Configure bootsrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 
 const App = () => (
-  <div className="text-sky-500">
-    Hello from app
+  <div className="text-sky-500 d-flex">
+    <p>Hello from app</p>
+    <p>Test bootsrap</p>
   </div>
 );
 
