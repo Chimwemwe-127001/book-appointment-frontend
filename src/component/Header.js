8d0f3fed69b57eb68a8b9e94635c66c5147852a5
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-29 17:11:25 +0530

    Add markupp for navigation

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,50 @@
 import React, { useState } from 'react';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+const menuItems = [
+  { name: 'Doctors', path: '/' },
+  { name: 'Reserve', path: '/' },
+  { name: 'My reservations', path: '/' },
+  { name: 'Add doctor', path: '/' },
+  { name: 'Delete doctor', path: '/' },
+];
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
   return (
-    <p></p>
-  )
-}
+    <header className={`flex flex-col bg-primaryColor px-8 py-7 ${mobileMenu ? 'h-screen' : 'h-fit'} fixed top-0 w-screen shadow-xl md:flex-row md:justify-between md:px-20 md:h-fit`}>
+      <a href="/" className="text-lg">Book Appoinments App </a>
+      <nav>
+        <div className="text-secondaryColor md:hidden mobile-btns">
+          <button
+            type="button"
+            className={classNames(!mobileMenu ? 'block' : 'hidden')}
+            onClick={() => setMobileMenu(true)}
+          >
+            <MenuIcon className="h-8" />
+          </button>
+          <button type="button" className={classNames(mobileMenu ? 'block' : 'hidden')} onClick={() => setMobileMenu(false)}>
+            <XIcon className="h-8" />
+          </button>
+        </div>
+        <div className={classNames(mobileMenu ? 'block' : 'hidden', 'md:block')}>
+          <ul className="flex flex-col text-skyColor text-lg gap-8 items-center mt-24 md:flex-row md:mt-1">
+            {
+              menuItems.map((item) => (
+                <li key={item.name}>
+                  <a href={item.path}>{item.name}</a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
