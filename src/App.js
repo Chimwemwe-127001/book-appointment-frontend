c3f7ca231d28d479e832672b82c84ca1416224ca
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-29 11:23:38 +0530

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 
-const App = ()=> {
-  return (
-    <div className="App">
-      Hello from app
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    Hello from app
+  </div>
+);
 
 export default App;
