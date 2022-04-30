aae3856c417a5419c5512a8d13e6904dc6a490ca
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-30 20:27:31 +0530

    Add social media icons

--- src/components/homepage/doctors.js
+++ src/components/homepage/doctors.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Paginate from './paginate';
+import insta from '../../assets/images/insta-icon-home.png'
+import twitter from '../../assets/images/twitter-icon-home.png'
+import facebook from '../../assets/images/fb-icon-home.png'
+
+
+const social = [
+  {icon: facebook},
+  {icon: twitter},
+  {icon: insta},
+]
 
 const Doctors = () => {
   const doctors = useSelector((state) => state.doctorsReducer);
@@ -17,13 +27,18 @@ const Doctors = () => {
   };
 
   return (
-    <div className="flex gap-4 mt-20">
+    <div className="flex gap-4 mt-16">
       {displayDoctors.map((item) => (
           <div className="flex flex-col items-center w-80">
          <img src={item.image} alt="doctor" className="rounded-full w-60 h-60" />
          <h3 className="uppercase text-slate-900 text-lg mt-6 font-bold">{item.name}</h3>
          <hr class="border-t-2 border-dashed border-gray-500 w-32 mt-4"></hr>
          <p className="text-gray-400 text-center font-semibold mt-4">{item.details}</p>
+         <div className="flex gap-4 mt-4">
+         {social.map((item) =>(
+             <a href="/"><img src={item.icon} alt="social" className="w-8"/></a>
+         ))}
+         </div>
          </div>
             ))}
       <Paginate pageCount={pageCount} changePage={changePage} />
