1e5a459478e16d222c0eda0348f49e36b02fa2b0
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-30 12:45:28 +0530

    Pass store in components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 );
