028cc5fc953cc9bbaf55fa17179e0c2b924c64ef
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-30 14:57:14 +0530

    Install react paginate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { useSelector } from 'react-redux';
-import SideBar from '../components/SideBar';
-import HamBurger from '../components/HamBurger';
+import SideBar from '../components/navigation/SideBar';
+import HamBurger from '../components/navigation/HamBurger';
 
 const Home = () =>
   // const doctors = useSelector((state) => state.doctorsReducer);
@@ -11,13 +11,14 @@ const Home = () =>
       <div className="hidden lg:flex lg:flex-shrink-0">
         <SideBar />
       </div>
-      <div className="flex flex-col min-w-0 flex-1 overflow-hidden mt-32 md:mt-1">
+      <div className="flex flex-col min-w-0 flex-1 overflow-hidden mt-32 lg:mt-1">
         {/* Hamburger for mobile */}
         <HamBurger />
         <div className="flex flex-col items-center w-screen lg:mt-32 lg:pr-44">
           <h1 className="text-3xl text-slate-900 uppercase font-bold">Available Doctors</h1>
           <p className="text-gray-500 mt-1 text-lg">Please select a doctor</p>
         </div>
+
       </div>
     </div>
   );
