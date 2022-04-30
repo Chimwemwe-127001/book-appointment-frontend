bc0004948b2c055e24108904af4b949e2de9f4ac
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-30 15:18:23 +0530

    Implement pagination

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import { useSelector } from 'react-redux';
 import SideBar from '../components/navigation/SideBar';
 import HamBurger from '../components/navigation/HamBurger';
+import Paginate  from 'react-paginate';
 
-const Home = () =>
-  // const doctors = useSelector((state) => state.doctorsReducer);
-  (
+const Home = () => {
+  const doctors = useSelector((state) => state.doctorsReducer);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const doctorsPerPage = 3;
+  const pageVisited = pageNumber* doctorsPerPage;
+  const displayDoctors = doctors.slice(pageVisited, pageVisited+doctorsPerPage);
+
+  const pageCount = Math.ceil(doctors.length / doctorsPerPage)
+
+  const changePage = ({selected}) =>{
+    setPageNumber(selected)
+  }
+
+  return (
     <div className="h-screen flex overflow-hidden bg-white">
       {/* Static sidebar for desktop */}
       <div className="hidden lg:flex lg:flex-shrink-0">
@@ -18,8 +31,16 @@ const Home = () =>
           <h1 className="text-3xl text-slate-900 uppercase font-bold">Available Doctors</h1>
           <p className="text-gray-500 mt-1 text-lg">Please select a doctor</p>
         </div>
+        <div>
+          {
+            displayDoctors.map((item) => (
+              <p>item.name</p>
+            ))
+          }
+        </div>
 
       </div>
     </div>
-  );
+  )
+};
 export default Home;
--- src/redux/doctors/doctors.js
+++ src/redux/doctors/doctors.js
@@ -1,6 +1,14 @@
 // state
 const initialState = [{
   name: 'Ranjeet',
+  name: 'Ranjeet2',
+  name: 'Ranjeet3',
+  name: 'Ranjeet4',
+  name: 'Ranjeet5',
+  name: 'Ranjeet6',
+  name: 'Ranjeet7',
+  name: 'Ranjeet8',
+  name: 'Ranjeet9',
 }];
 
 // reducer
