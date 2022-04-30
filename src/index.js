f766ce686b2539eaca4a9aa4a484cdfd696228f0
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-30 12:46:14 +0530

    Setup base routes in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <Router>
     <App />
+    </Router>
     </Provider>
   </React.StrictMode>,
 );
