172fd1fec2b871d63735b20bb687da6d7bca0162
Anselem <anselemodims@gmail.com>
2022-05-02 09:37:14 +0100

    Feat: import react router dom and define paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 
 const App = () => (
   <div className="text-sky-500 d-flex">
+    <Router>
+      <Routes>
+        {/* <Route element={<Login />} >
+          <Route path="/" element={
+            <PrivateRoute >
+              <DashBoard />
+            </PrivateRoute>
+          } />
+        </Route> */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
     <Home />
   </div>
 );
