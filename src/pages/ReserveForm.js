089b5e0b1b89f76f0b0344dc5cdfa6564cc96fbe
Buyaki <rittahbuyaki@gmail.com>
2022-05-02 04:19:15 +0300

    Add the hamburger and search icon

---
+++ src/pages/ReserveForm.js
@@ -0,0 +1,11 @@
+import { FaBars, FaSistrix,FaAngleDown } from "react-icons/fa"
+
+const ReserveForm = () => (
+  <div>
+    <div className="flex justify-between">
+      <button> <FaBars/> </button>
+      <button> <FaSistrix/> </button>
+    </div>
+    <button></button>
+  </div> 
+)
