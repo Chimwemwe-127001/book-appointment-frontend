6498768fcfd4520192b72caa322304a09640c7b1
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-04 23:52:44 +0530

    Update reducer

--- src/redux/doctors/doctors.js
+++ src/redux/doctors/doctors.js
@@ -1,82 +1,42 @@
+import Axios from 'axios';
+
+// conts
+const FETCH_DOCTORS = 'BOOK-APPOINTMENT/DOCTORS/FETCH_DOCTORS';
+
+// actions
+const fetchRocket = (payload) => ({
+  type: FETCH_DOCTORS,
+  payload,
+});
+
 // state
-const initialState = [
-  {
-    name: 'Ranjeet',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLF5Kd7k3U7K1A2s7Xqqj1DLpLvvIFPdYZIQ&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Anesthesiology',
-    city: 'Mexico',
-    cost: 12000,
-  },
-  {
-    name: 'Ranjeet2',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRPvzxHDfn24X9oAiPUVMwhsQ15ir5Kq2mWw&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Dermatology',
-    city: 'Buenos Aires',
-    cost: 40000,
-  },
-  {
-    name: 'Ranjeet3',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3IAmy6NE_XDi22Unc6HnVnKNzUmejPilKPQ&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Optician',
-    city: 'Copenhagen',
-    cost: 59000,
-  },
-  {
-    name: 'Ranjeet4',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmqvkUkpAyPJlfA33wBdfx-Z_bYmXSkOOwUA&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Medical genetics',
-    city: 'Oklahoma City',
-    cost: 28000,
-  },
-  {
-    name: 'Ranjeet5',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMr2ztQlgpJjFGcxrjou7eoDmJs5aw2PT7XA&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Diagnostic radiology',
-    city: 'Dubai',
-    cost: 41000,
-  },
-  {
-    name: 'Ranjeet6',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRPvzxHDfn24X9oAiPUVMwhsQ15ir5Kq2mWw&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Neurology',
-    city: 'Riyadh',
-    cost: 30000,
-  },
-  {
-    name: 'Ranjeet7',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLF5Kd7k3U7K1A2s7Xqqj1DLpLvvIFPdYZIQ&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Ophthalmology',
-    city: 'Beijing',
-    cost: 22000,
-  },
-  {
-    name: 'Ranjeet8',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMr2ztQlgpJjFGcxrjou7eoDmJs5aw2PT7XA&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Obstetrics and gynecology',
-    city: 'Luxembourg',
-    cost: 16000,
-  },
-  {
-    name: 'Ranjeet9',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmqvkUkpAyPJlfA33wBdfx-Z_bYmXSkOOwUA&usqp=CAU',
-    details: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-    specialization: 'Urology',
-    city: 'Boston',
-    cost: 49000,
-  },
-];
+const initialState = [];
+
+//   APIs-functions
+
+export const fetchRocketApi = () => async (dispatch) => {
+  const returnValue = await Axios.get('https://api.spacexdata.com/v3/rockets');
+  const { data } = returnValue;
+  const rockets = [];
+  for (let i = 0; i < data.length; i += 1) {
+    const name = data[i].rocket_name;
+    const { id } = data[i];
+    const { description } = data[i];
+    const image = data[i].flickr_images[0];
+    const reserved = false;
+    const object = {
+      id, name, description, image, reserved,
+    };
+    rockets.push(object);
+  }
+  dispatch(fetchRocket(rockets));
+};
 
 // reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_DOCTORS:
+      return action.payload;
     default:
       return state;
   }
