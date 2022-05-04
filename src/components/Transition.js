6305993d0a135e4c4e0c9d120632178af1a8761a
Anselem <anselemodims@gmail.com>
2022-05-04 20:55:58 +0100

    Feat: include an animation to the transtion component

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,7 +1,26 @@
-const Transition = () => (
-  <div>
-    <div className="transition-effect" />
-  </div>
-);
+import { Power4 } from 'gsap';
+import { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+const Transition = ({ timeline }) => {
+  const trans = useRef(null);
+  useEffect(() => {
+    timeline.to(trans.current, {
+      duration: 20,
+      y: -2000,
+      ease: Power4.easeOut,
+      delay: 1,
+    });
+  });
+  return (
+    <div>
+      <div className="transition-effect" ref={trans} />
+    </div>
+  );
+};
+
+Transition.propTypes = {
+  timeline: PropTypes.func.isRequired,
+};
 
 export default Transition;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 import SideBar from '../components/navigation/SideBar';
 import HamBurger from '../components/navigation/HamBurger';
 import Doctors from '../components/homepage/doctors';
+import Transition from '../components/Transition';
 
-const Home = () => (
-  <div className="h-screen flex lg:overflow-hidden bg-white">
-    {/* Static sidebar for desktop */}
-    <div className="hidden lg:flex lg:flex-shrink-0">
-      <SideBar />
-    </div>
-    <div className="flex flex-col min-w-0 flex-1 lg:overflow-hidden mt-32 lg:mt-1">
-      {/* Hamburger for mobile */}
-      <HamBurger />
-      <div className="flex flex-col items-center w-screen lg:mt-24 lg:pr-44">
-        <h1 className="text-3xl text-slate-900 uppercase font-bold">Available Doctors</h1>
-        <p className="text-gray-500 mt-1 text-lg">Please select a doctor</p>
+const Home = () => {
+  const home = gsap.timeline();
+  const homedoc = useRef(null);
+  // const homeimg = useRef(null);
+
+  useEffect(() => {
+    gsap.from(homedoc.current, {
+      y: -1000,
+      delay: 1,
+    });
+    gsap.to(homedoc.current, {
+      y: 0,
+      delay: 1,
+      ease: 'power4.out',
+      duration: 6,
+    });
+  }, []);
+
+  return (
+    <div className="homepage">
+      <Transition timeline={home} />
+      <div className="h-screen flex lg:overflow-hidden bg-white" ref={homedoc}>
+        {/* Static sidebar for desktop */}
+        <div className="hidden lg:flex lg:flex-shrink-0">
+          <SideBar />
+        </div>
+        <div className="flex flex-col min-w-0 flex-1 lg:overflow-hidden mt-32 lg:mt-1">
+          {/* Hamburger for mobile */}
+          <HamBurger />
+          <div className="flex flex-col items-center w-screen lg:mt-24 lg:pr-44">
+            <h1 className="text-3xl text-slate-900 uppercase font-bold">Available Doctors</h1>
+            <p className="text-gray-500 mt-1 text-lg">Please select a doctor</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Doctors />
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col items-center">
-        <Doctors />
-      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Home;
