7d8c6432b140b0084a60c9d73d6aa866b629fa22
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-05 11:36:56 +0530

    Add reservations reducer

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import SideBar from '../components/navigation/SideBar';
 import HamBurger from '../components/navigation/HamBurger';
+import Reservation from '../components/reservations/Reservation';
 
 const Reservations = () => (
-  <div className="h-screen flex lg:overflow-hidden bg-white">
+  <div className="h-screen flex bg-white">
     {/* Static sidebar for desktop */}
     <div className="hidden lg:flex lg:flex-shrink-0">
       <SideBar />
     </div>
-    <div className="flex flex-col min-w-0 flex-1 lg:overflow-hidden mt-32 lg:mt-1">
+    <div className="flex flex-col min-w-0 flex-1 mt-32 lg:mt-1">
       {/* Hamburger for mobile */}
       <HamBurger />
       <div className="flex flex-col items-center w-screen lg:mt-4 lg:pr-44">
         <h1 className="text-3xl text-slate-900 uppercase font-bold">All Reservations</h1>
       </div>
       <div className="flex flex-col items-center">
-        Hello
+        <Reservation />
       </div>
     </div>
   </div>
