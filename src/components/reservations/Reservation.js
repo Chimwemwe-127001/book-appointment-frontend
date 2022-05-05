39908119b843627c33f55791ad9460b8ea78e226
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-05 11:41:39 +0530

    Call user and doctors state in reservations

--- src/components/reservations/Reservation.js
+++ src/components/reservations/Reservation.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 
 const Reservation = () =>{
+  const user = useSelector((state) => state.signUpReducer);
+  const { accessToken } = user;
+  const doctors = useSelector((state) => state.doctorsReducer);
+
   return (
     <p>Hello</p>
   )
