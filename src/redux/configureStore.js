1d201863dceb009f993f57a733f25ca7a3b88bfe
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-05 11:38:24 +0530

    Pass reservations reducer pass in root reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,10 +3,12 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import doctorsReducer from './doctors/doctors';
 import signUpReducer from './session/reducer/signUpReducer';
+import reservationsReducer from './reservations/reservations'; 
 
 const reducer = combineReducers({
   doctorsReducer,
   signUpReducer,
+  reservationsReducer,
 });
 
 const store = createStore(
---
+++ src/redux/reservations/reservations.js
@@ -0,0 +1,40 @@
+import Axios from 'axios';
+
+// URLs
+const BASE_URL = 'http://localhost:3000/api/v1';
+
+// conts
+const FETCH_DOCTORS = 'BOOK-APPOINTMENT/DOCTORS/FETCH_DOCTORS';
+
+// actions
+const fetchDoctors = (payload) => ({
+  type: FETCH_DOCTORS,
+  payload,
+});
+
+// state
+const doctorsState = [];
+
+//   APIs-functions
+
+export const fetchDoctorsApi = (accessToken) => async (dispatch) => {
+  const returnValue = await Axios.get(`${BASE_URL}/doctors`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const { doctors } = returnValue.data.data;
+  dispatch(fetchDoctors(doctors));
+};
+
+// reducer
+const reducer = (state = doctorsState, action) => {
+  switch (action.type) {
+    case FETCH_DOCTORS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
