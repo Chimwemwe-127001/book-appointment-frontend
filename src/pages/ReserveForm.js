fb50b3a0d325223190dfaa6ff0164e94fbc41d9c
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-06 12:59:48 +0530

    Fix rservation form

--- src/pages/ReserveForm.js
+++ src/pages/ReserveForm.js
@@ -27,13 +27,14 @@ const ReserveForm = () => (
       </div>
       <div className="mt-5 flex justify-center">
         <div className="flex flex-col lg:block">
-          <input type="text" placeholder="City" className="lg:mr-5 mb-4 p-3 bg-lime-500 rounded-lg outline outline-offset-2 outline-3" />
-          <select name="availableDoctors" id="availableDoctors" className="lg:ml-3 lg:mr-5 p-3 bg-lime-500 rounded-lg outline outline-offset-2 outline-3">
+          <input type="text" placeholder="City" required className="lg:mr-5 mb-4 p-3 bg-lime-500 rounded-lg outline outline-offset-2 outline-3" />
+          <select name="availableDoctors" required id="availableDoctors" className="lg:ml-3 lg:mr-5 p-3 bg-lime-500 rounded-lg outline outline-offset-2 outline-3">
+            <option value="" selected disabled hidden>Choose here</option>
             <option value="Ranjeet"> Dr.Ranjeet1 </option>
             <option value="Ranjeet"> Dr.Ranjeet2 </option>
             <option value="Ranjeet"> Dr.Ranjeet3 </option>
           </select>
-          <input type="date" value="Date" className="text-white mt-6 lg:mt-0 lg:mr-10 p-3 bg-lime-500 rounded-lg outline outline-offset-2 outline-3" />
+          <input type="date" value="Date" required className="text-white mt-6 lg:mt-0 lg:mr-10 p-3 bg-lime-500 rounded-lg outline outline-offset-2 outline-3" />
           <br />
           <div className="flex flex-col items-center lg:grow">
             <input type="submit" value="Book Now" className="mt-4 text-lime-500 bg-white p-3 rounded-lg outline outline-offset-2 outline-3" />
