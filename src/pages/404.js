572a0b7ba022c1a1d98b003d9adfb79a345ceaef
Anselem <anselemodims@gmail.com>
2022-05-06 15:36:29 +0100

    Feat: style the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => (
-  <section className="not-found">Error, Not found</section>
+  <section className="not-found">
+    <div>
+      <p>Lost your way?</p>
+      <span>
+        Sorry, we can&apos;t find that page. You can find doctors in the
+        homepage and start booking appointments
+      </span>
+    </div>
+    <div>
+      <Link to="/">Go Home</Link>
+    </div>
+  </section>
 );
 
 export default NotFound;
