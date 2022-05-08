d0fc273c43f4618d5b2bedfa8fc4818c68514a86
mwanawabangona <ellisallday@gmail.com>
2022-05-08 09:44:59 +0200

    Add Imports

---
+++ src/pages/DeleteDoctors.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import SideBar from '../components/navigation/SideBar';
+import HamBurger from '../components/navigation/HamBurger';
+import DeleteDoctor from '../components/deletedoctors/DeleteDoctor'
