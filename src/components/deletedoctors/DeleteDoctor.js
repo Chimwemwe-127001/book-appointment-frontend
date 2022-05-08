60d2d26a46c47ba5d5b67facf552abd8edf16131
mwanawabangona <ellisallday@gmail.com>
2022-05-08 09:49:58 +0200

    Add delete Function

--- src/components/deletedoctors/DeleteDoctor.js
+++ src/components/deletedoctors/DeleteDoctor.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteDoctorApi } from '../../redux/doctors/doctors';
+
+const DeleteDoctor=() =>{
+    const doctors = useSelector((state) => state.doctorsReducer);
+    const user = useSelector((state) => state.signUpReducer);
+    const { accessToken } = user;
+  
+    const dispatch = useDispatch();
+    const deleteDoctor = (id) => {
+      dispatch(deleteDoctorApi(accessToken, id));
+    };
