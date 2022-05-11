dc8f22f34b3119cf208eddca6f9a0e7f3e7d81ac
mwanawabangona <ellisallday@gmail.com>
2022-05-11 02:56:48 +0200

    Update docform

--- src/pages/DoctorForm.js
+++ src/pages/DoctorForm.js
@@ -13,10 +13,8 @@ const DoctorForm = () => (
       <div className="flex flex-col min-w-0 flex-1 lg:overflow-hidden mt-32 lg:mt-1">
         {/* Hamburger for mobile */}
         <HamBurger />
-        <div className="flex flex-col items-center w-screen lg:mt-24 lg:pr-44">
-          <h1 className="text-3xl text-slate-900 uppercase font-bold">Add a doctor</h1>
-        </div>
-        <div className="flex flex-col items-center">
+        
+        <div className="flex flex-col items-center doc">
           <AddDoctor />
         </div>
       </div>
