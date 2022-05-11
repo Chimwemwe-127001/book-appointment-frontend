0ec43f881e523a7e07284678e74fead5bf7281d6
mwanawabangona <ellisallday@gmail.com>
2022-05-11 02:57:50 +0200

    Update app js

--- src/App.js
+++ src/App.js
@@ -55,9 +55,9 @@ const App = () => (
         <Route
           path="/doctorform"
           element={(
-            <PrivateRoute>
+            // <PrivateRoute>
               <DoctorForm />
-            </PrivateRoute>
+            // </PrivateRoute>
             )}
         />
         <Route
